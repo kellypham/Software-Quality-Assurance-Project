c25cc1e8f8a1c5ca76583082269d78630c3cba41
Tamilesh <tamilselvan.balasuntharam@ontariotechu.net>
2020-03-10 15:10:31 -0400

    User class still needs testing

--- AuctionrBack/Models/User.java
+++ AuctionrBack/Models/User.java
@@ -1,6 +1,79 @@
 package AuctionrBack.Models;
 
+/** Enum for user types */
+enum UserType{
+    ADMIN,
+    FULL_STANDARD,
+    BUY_STANDARD,
+    SELL_STANDARD
+}
+
+/**
+ * Class storing user data
+ */
 public class User
 {
-	
+	private String name;
+    private UserType type;
+    private int credit;
+    /**
+    * Accessor for user's name.
+    * @return User's name
+    */
+    public String GetName(){
+        return this.name;
+    }
+    /**
+    * Mutator for user's name
+    * @param name New name value
+    */
+    public void SetName(String name){
+        this.name = name;
+    }
+
+
+    /**
+    * Accessor for user's type.
+    * @return User's type from UserTypes enum
+    */
+    public UserType GetType(){
+        return this.type;
+    }
+    /**
+    * Mutator for user's type
+    * @param name New user type
+    */
+    public void SetType(UserType type){
+        this.type = type;
+    }
+
+    /**
+    * Accessor for user's credit.
+    * @return User's credit
+    */
+    public int GetCredit(){
+        return this.credit;
+    }
+
+    /**
+    * Mutator for user's credit
+    * @param name New credit value
+    */
+    public SetCredit(int credit){
+        this.credit = credit;
+    }
+
+    /**
+    * Accessor for user's admin type.
+    * @return True if User is an Admin
+    */
+    public boolean IsAdmin(){
+        if(this.type == UserType.ADMIN){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
 }
