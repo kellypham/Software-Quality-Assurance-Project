fc563be4171e87975f6a3f0d9c682bf2838406d6
kellypham <46714872+kellypham@users.noreply.github.com>
2020-03-14 23:50:20 -0400

    Add files via upload

---
+++ AuctionrBack/Commands/Implementation/AddCredit.java
@@ -0,0 +1,19 @@
+package AuctionrBack.Commands.Implementation;
+
+import AuctionrBack.Commands.Command;
+
+public class AddCredit extends Command {
+
+    public AddCredit(String[] args){
+		super(args);
+    }
+
+    public void Validate(){
+        
+    }
+
+    public void Execute(){
+
+    }
+
+}
