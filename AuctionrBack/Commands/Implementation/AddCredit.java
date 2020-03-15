70688fa8fcecf194d6c3c81983af105c21dc02ca
kellypham <46714872+kellypham@users.noreply.github.com>
2020-03-15 15:40:09 -0400

    Update AddCredit.java

--- AuctionrBack/Commands/Implementation/AddCredit.java
+++ AuctionrBack/Commands/Implementation/AddCredit.java
@@ -3,7 +3,6 @@ package AuctionrBack.Commands.Implementation;
 import AuctionrBack.Commands.Command;
 import AuctionrBack.Models.*;
 import AuctionrBack.Storage.UserStorage;
-import AuctionrBack.Storage.Exceptions.*;
 
 public class AddCredit extends Command {
 	
@@ -16,7 +15,7 @@ public class AddCredit extends Command {
 		super(args);
     }
 
-    public void Validate() throws MyException, UserNotFoundException {
+    public void Validate() throws Exception{
     	String newcredit = this.args[1];
     	String userName = this.args[2];
     	User user = this.userStorage.GetByName(userName);
@@ -24,17 +23,17 @@ public class AddCredit extends Command {
     	
     	//User must be an admin account
     	if (type.toString() != "AA") {
-    		throw new MyException("Error: User must be an admin account");
+    		throw new Exception("Error: User must be an admin account");
     	}
     	
     	//A maximum of $1000.00 can be added to an account 
     	if (Integer.parseInt(newcredit) > maxCredit) {
-    		throw new MyException("Error: The maximum credit is $1000.00");
+    		throw new Exception("Error: The maximum credit is $1000.00");
     	}
     }
 
     //add credit into the system for the purchase of accounts
-    public void Execute() throws ItemNotFoundException, UserNotFoundException{
+    public void Execute() throws Exception{
     	//Args Variable
         String newcredit = this.args[1];
         String userName = this.args[2];
