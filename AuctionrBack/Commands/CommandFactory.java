e8348b533589b584c07f1f82c566d884406f6ea3
Anthony Kouroupis <anthony.kouroupis@ontariotechu.net>
2020-03-15 18:05:59 -0400

    Updated CommandFactory to return commands

--- AuctionrBack/Commands/CommandFactory.java
+++ AuctionrBack/Commands/CommandFactory.java
@@ -2,9 +2,18 @@ package AuctionrBack.Commands;
 
 import AuctionrBack.Input.LogEntry;
 import AuctionrBack.Storage.*;
+import AuctionrBack.Commands.Implementation.*;
 
 public class CommandFactory
 {
+	private static final String CODE_CREATE = "01";
+	private static final String CODE_ADD_CREDIT = "06";
+	private static final String CODE_ADVERTISE = "03";
+	private static final String CODE_BID = "04";
+	private static final String CODE_DELETE = "02";
+	private static final String CODE_REFUND = "05";
+
+
 	private UserStorage userStorage;
 	private ItemStorage itemStorage;
 
@@ -16,6 +25,34 @@ public class CommandFactory
 
 	public Command Create(LogEntry entry)
 	{
-		return null;
+		String code = entry.TransactionCode();
+		String[] args = entry.Arguments();
+
+		if (code == CODE_CREATE)
+		{
+			return new AddCredit(args, userStorage);
+		}
+		else if (code == CODE_DELETE)
+		{
+			return new DeleteCommand(args, userStorage, itemStorage)
+		}
+		else if (code == CODE_ADD_CREDIT)
+		{
+			return new AddCredit(args, userStorage);
+		}
+		else if (code == CODE_ADVERTISE)
+		{
+			return new Advertise(args);
+		}
+		else if (code == CODE_BID)
+		{
+			return new Bid(args);
+		}
+		else if (code == CODE_REFUND)
+		{
+			return new Refund(args);
+		}
+
+		return null;	
 	}
 }
