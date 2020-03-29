db11c4d206b2ef88fb3881df1751a738777c0deb
Anthony Kouroupis <anthony@ubiquenet.com>
2020-03-29 03:38:21 -0400

    CommandFactory is now tested

---
+++ AuctionrBack/Tests/CommandFactoryTest.java
@@ -0,0 +1,83 @@
+package AuctionrBack.Tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+
+import AuctionrBack.Commands.Command;
+import AuctionrBack.Commands.CommandFactory;
+import AuctionrBack.Commands.Implementation.AddCredit;
+import AuctionrBack.Commands.Implementation.Advertise;
+import AuctionrBack.Commands.Implementation.Bid;
+import AuctionrBack.Commands.Implementation.CreateCommand;
+import AuctionrBack.Commands.Implementation.DeleteCommand;
+import AuctionrBack.Commands.Implementation.Refund;
+import AuctionrBack.Input.LogEntry;
+
+public class CommandFactoryTest
+{
+	@Test
+	public void ReturnAddCredit()
+	{
+		RunTest("06", AddCredit.class);
+	}
+
+	@Test
+	public void ReturnAdvertise()
+	{
+		RunTest("03", Advertise.class);
+	}
+
+	@Test
+	public void ReturnBid()
+	{
+		RunTest("04", Bid.class);
+	}
+
+	@Test
+	public void ReturnCreate()
+	{
+		RunTest("01", CreateCommand.class);
+	}
+
+	@Test
+	public void ReturnDelete()
+	{
+		RunTest("02", DeleteCommand.class);
+	}
+
+	@Test
+	public void ReturnRefund()
+	{
+		RunTest("05", Refund.class);
+	}
+
+	@Test
+	public void ReturnNull()
+	{
+		LogEntry entry = new LogEntry("NonExistentCode", new String[] {});
+		CommandFactory factory = new CommandFactory(null, null);
+
+		Command cmd = factory.Create(entry);
+
+		assertEquals(null, cmd);
+	}
+	
+	public void RunTest(String commandCode, Class expectedType)
+	{
+		LogEntry entry = new LogEntry(commandCode, new String[] {"0", "0", "0", "0"});
+		CommandFactory factory = new CommandFactory(null, null);
+
+		Command cmd = factory.Create(entry);
+
+		assertNotEquals("Factory returned unexpected null for code " + commandCode,
+			null, cmd
+		);
+		assertEquals(
+			"Command code " + commandCode + " did not return type " + expectedType.getName(),
+			expectedType, cmd.getClass()
+		);
+	}
+
+}
